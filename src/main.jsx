0336c947504cc621fbb5f0793b2918b7ad8e8aa4
Tri Phan <tripp@ames.edu.vn>
2023-02-15 14:47:10 +0700

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import NorthStarApp from './projects/northstar/NorthStarApp'
+
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<NorthStarApp />
+	</React.StrictMode>
 )
---
+++ src/projects/northstar/NorthStarApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MyComponent from './components/MyComponent'
+
+const NorthStarApp = () => {
+	return <MyComponent />
+}
+
+export default NorthStarApp
